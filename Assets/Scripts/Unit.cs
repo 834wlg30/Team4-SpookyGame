6ff7fc40f7ef774e9a63a82300f8dc025cabcff3
834wlg30 <wlgulick@sbcglobal.net>
2021-10-13 18:39:01 -0500

    Added Unit and GameManager Scripts
    Implemented unit selection

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public GameObject gmObj;
+
+    private void Awake()
+    {
+        gmObj = GameObject.FindGameObjectWithTag("GM");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Player Clicked");
+        List<GameObject> unitList = gmObj.GetComponent<GameManager>().selectedUnits;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if(!unitList.Contains(this.gameObject))
+            unitList.Add(this.gameObject);
+        }
+        else
+        {
+            unitList.RemoveRange(0, unitList.Count);
+            unitList.Add(this.gameObject);
+
+        }
+    }
+}
