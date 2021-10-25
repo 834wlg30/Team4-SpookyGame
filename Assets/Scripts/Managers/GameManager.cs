d5ed087bbbe74e4d04361784a61103cce87db2b6
834wlg30 <wlgulick@sbcglobal.net>
2021-10-25 18:19:28 -0500

    Asset Pull + A* imported

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
 
     public List<GameObject> selectedUnits = new List<GameObject>();
+    public Vector3 mousePos;
+    public Vector3 stwPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,22 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos = Input.mousePosition;
+        DetectCommand();
+    }
+
+    private void DetectCommand()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("RMB Down");
+            foreach (GameObject unit in selectedUnits)
+            {
+                stwPoint = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 1));
+                Debug.Log(stwPoint);
+                Unit unitScript = unit.GetComponent<Unit>();
+                unitScript.Move(stwPoint);
+            }
+        }
     }
 }
