eb87780dd8437ff20750d855535ce511abc944ca
Smoklen <jep082@yahoo.com>
2021-10-27 19:33:52 -0500

    GodHelpUs.cs

--- Assets/Scripts/GodHelpUs.cs
+++ Assets/Scripts/GodHelpUs.cs
@@ -7,7 +7,7 @@ public class GodHelpUs : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("GodHelpUs.cs");
     }
 
     // Update is called once per frame
